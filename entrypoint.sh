a999c0ca6b3f35a06d0ea98c345d6a4148396417
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2022-11-01 11:27:09 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,18 +50,6 @@ current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 pre_release="$prerelease"
 IFS=',' read -ra branch <<< "$release_branches"
-for b in "${branch[@]}"; do
-    # check if ${current_branch} is in ${release_branches} | exact branch match
-    if [[ "$current_branch" == "$b" ]]
-    then
-        pre_release="false"
-    fi
-    # verify non specific branch names like  .* release/* if wildcard filter then =~
-    if [ "$b" != "${b//[\[\]|.? +*]/}" ] && [[ "$current_branch" =~ $b ]]
-    then
-        pre_release="false"
-    fi
-done
 echo "pre_release = $pre_release"
 
 # fetch tags
